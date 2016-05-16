c2979f382fc4c5399db3d3207e65f5fd7027478b
Tong Tung Giang <tongtunggiang@gmail.com>
2016-05-16 10:22:24 +0700

    Add Input System module in Application Layer

--- Engine/Subsystems/ApplicationLayer/InputSystem.cpp
+++ Engine/Subsystems/ApplicationLayer/InputSystem.cpp
@@ -1,6 +1,34 @@
 #include "InputSystem.h"
 
+namespace MagEngine
+{
+
 InputSystem::InputSystem()
 {
+}
+
+InputSystem& InputSystem::operator = (const InputSystem&)
+{
+}
+
+bool InputSystem::keyPressed(const OIS::KeyEvent &e)
+{
+}
+
+bool InputSystem::keyReleased(const OIS::KeyEvent &e)
+{
+}
+
+bool InputSystem::mouseMoved(const OIS::MouseEvent &e)
+{
+}
+
+bool InputSystem::mousePressed(const OIS::MouseEvent &e, OIS::MouseButtonID id)
+{
+}
+
+bool InputSystem::mouseReleased(const OIS::MouseEvent &e, OIS::MouseButtonID id)
+{
+}
 
 }
--- Engine/Subsystems/ApplicationLayer/InputSystem.h
+++ Engine/Subsystems/ApplicationLayer/InputSystem.h
@@ -19,7 +19,58 @@ class InputSystem :public OIS::KeyListener, public OIS::MouseListener, public OI
 
 public:
 
+    virtual ~InputSystem();
+
+    void initialise(Ogre::RenderWindow *renderWindow);
+    void capture();
+
+    void addKeyListener(OIS::KeyListener *keyListener, const std::string& instanceName);
+    void addMouseListener(OIS::MouseListener *mouseListener, const std::string& instanceName);
+
+    void removeKeyListener(const std::string& instanceName);
+    void removeMouseListener(const std::string& instanceName);
+
+    void removeKeyListener(OIS::KeyListener *keyListener);
+    void removeMouseListener(OIS::MouseListener *mouseListener);
+
+    void removeAllListeners();
+    void removeAllKeyListeners();
+    void removeAllMouseListeners();
+
+    void setWindowExtents(int width, int height);
+
+    Mouse*    getMouse();
+    Keyboard* getKeyboard();
+
+    static InputSystem* getSingletonPtr();
+
+private:
+
     InputSystem();
+    InputSystem(const InputSystem&) { }
+    InputSystem& operator = (const InputSystem&);
+
+    bool keyPressed(const OIS::KeyEvent &e);
+    bool keyReleased(const OIS::KeyEvent &e);
+
+    bool mouseMoved(const OIS::MouseEvent &e);
+    bool mousePressed(const OIS::MouseEvent &e, OIS::MouseButtonID id);
+    bool mouseReleased(const OIS::MouseEvent &e, OIS::MouseButtonID id);
+
+    OIS::Mouse *mouse;
+    OIS::Keyboard *keyboard;
+    OIS::InputManager *ogreInputManager;
+
+    std::map<std::string, OIS::KeyListener*> mKeyListeners;
+    std::map<std::string, OIS::MouseListener*> mMouseListeners;
+
+    std::map<std::string, OIS::KeyListener*>::iterator itKeyListener;
+    std::map<std::string, OIS::MouseListener*>::iterator itMouseListener;
+
+    std::map<std::string, OIS::KeyListener*>::iterator itKeyListenerEnd;
+    std::map<std::string, OIS::MouseListener*>::iterator itMouseListenerEnd;
+
+    static InputSystem *inputSystem;
 
 };
 
